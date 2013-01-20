d7457369db0abf4039579cd819e4012a838a79c9
aash <aash@thyme.ashfamily.local>
2013-01-19 21:57:09 -0500

    Finished first half of Ch. 1 (up to "Deploying an Application")

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -9,5 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface QuizViewController : UIViewController
+{
+    int currentQuestionIndex;
+    
+    // The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    // The view objects - don't worry about IBOutlet
+    //   we'll talk about it shortly
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -14,16 +14,75 @@
 
 @implementation QuizViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self) {
+        // Create two arrays and make the pointers point to them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        // Add questions and answers to the arrays
+        [questions addObject:@"The Mojito contains which herb?"];
+        [answers addObject:@"Mint"];
+        
+        [questions addObject:@"Gin is made from which berry?"];
+        [answers addObject:@"the juniper berry"];
+        
+        [questions addObject:@"Ari's favourite beer is..."];
+        [answers addObject:@"Newcastle. Brilliant!"];
+    }
+    
+    // Return the address of the new object
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)showQuestion:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Step to the next question
+    currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (currentQuestionIndex == [questions count]) {
+
+        // Go back to the first question
+        currentQuestionIndex = 0;
+    }
+    
+    // Get the string at the index in the questions array
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    // Log the string to the console
+    NSLog(@"displaying question: %@", question);
+    
+    // Displaying the string int he question field
+    [questionField setText:question];
+    
+    // Clear the answer field
+    [answerField setText:@"???"];
 }
+
+
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    
+    // Display it in the answer field
+    [answerField setText:answer];
+}
+
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//	// Do any additional setup after loading the view, typically from a nib.
+//}
+//
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 @end
