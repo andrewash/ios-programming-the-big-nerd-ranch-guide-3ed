500c654963ee8eae32b6b0bece9da7ede6e250ec
aash <aash@thyme.ashfamily.local>
2013-01-20 22:57:50 -0500

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by aash on 2013-01-20.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
