9cc594c0a266b3e257be942cdfb57d6b591346b9
aash <aash@thyme.ashfamily.local>
2013-01-20 23:39:58 -0500

    Readings, Ch. 2, read 1/3 in 60mins. Hope to read faster next time :-)

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -10,12 +10,28 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+ 
+        // Send the message addObject: to the NSMutableArray pointed to by
+        //  the variable items, passing a string each time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Send another message, insertObject:atIndex:, to the array
+        [items insertObject:@"Zero" atIndex:0];
         
+        // For every item in the array as determined by sending count to items...
+        for (int i=0; i < [items count]; i++) {
+            // We get the ith object from the array and pass it as an argument to NSLog
+            //  which implicitly sends the description message to that object
+            NSLog(@"%@", [items objectAtIndex:i]);
+        }
+        
+        // Destroy the array pointed to by items
+        items = nil;
     }
     return 0;
 }
