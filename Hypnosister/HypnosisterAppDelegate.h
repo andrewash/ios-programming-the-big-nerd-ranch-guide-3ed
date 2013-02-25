35ed1a342370ff6af125bd2c41f3317afd6c131d
aash <aash@thyme.local>
2013-02-25 18:46:35 -0500

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by aash on 2013-02-25.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
