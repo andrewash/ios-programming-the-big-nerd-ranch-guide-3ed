fb890174a17ebe29134103a12df90c8c11000389
Andrew Ash <enderash@gmail.com>
2013-03-03 15:27:23 -0500

    Initial Commit

---
+++ HypnoTime/HypnoTimeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoTimeAppDelegate.h
+//  HypnoTime
+//
+//  Created by aash on 2013-03-03.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoTimeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by aash on 2013-03-03.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoTimeAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoTimeAppDelegate class]));
+    }
+}
