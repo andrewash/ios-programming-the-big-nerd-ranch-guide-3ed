6ac863bd9e1f855db2cd55cba1948d71cb578c97
Andrew Ash <enderash@gmail.com>
2013-03-10 11:45:21 -0400

    minor update to comments at top of these files

--- HeavyRotation/RotationAppDelegate.h
+++ HeavyRotation/RotationAppDelegate.h
@@ -1,10 +1,7 @@
 //
 //  RotationAppDelegate.h
-//  HeavyRotation
-//
 //  Created by aash on 2013-03-10.
-//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- HeavyRotation/RotationAppDelegate.m
+++ HeavyRotation/RotationAppDelegate.m
@@ -1,10 +1,7 @@
 //
 //  RotationAppDelegate.m
-//  HeavyRotation
-//
 //  Created by aash on 2013-03-10.
-//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
-//
+
 
 #import "RotationAppDelegate.h"
 
