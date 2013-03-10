7892ed31089a209fa27d0eef6e9f239175ec34e4
Andrew Ash <enderash@gmail.com>
2013-03-10 11:44:55 -0400

    Added new UIViewController w/ matching XIB file - minor update to comments at top

---
+++ HeavyRotation/RotationViewController.h
@@ -0,0 +1,10 @@
+//
+//  RotationViewController.h
+//  Created by aash on 2013-03-10.
+
+
+#import <UIKit/UIKit.h>
+
+@interface RotationViewController : UIViewController
+
+@end
---
+++ HeavyRotation/RotationViewController.m
@@ -0,0 +1,35 @@
+//
+//  RotationViewController.m
+//  Created by aash on 2013-03-10.
+
+
+#import "RotationViewController.h"
+
+@interface RotationViewController ()
+
+@end
+
+@implementation RotationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
