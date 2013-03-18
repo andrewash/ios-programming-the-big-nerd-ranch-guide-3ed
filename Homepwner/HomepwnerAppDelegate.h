c14cae7237c4571d589363406b57e59cdd0a3d8d
Andrew Ash <enderash@gmail.com>
2013-03-18 17:56:07 -0400

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by aash on 2013-03-18.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
