05a3833952cda9cc575781ede3306c55e2516537
Andrew Ash <enderash@gmail.com>
2013-03-20 20:05:26 -0400

    our view controller now conforms to the UITableViewDataSource protocol

--- Homepwner/ItemsViewController.m
+++ Homepwner/ItemsViewController.m
@@ -24,4 +24,25 @@
 - (id)initWithStyle:(UITableViewStyle)style {
     return [self init];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section {
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Create an instance of UITableViewCell, with default appearance
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                   reuseIdentifier:@"UITableViewCell"];
+    
+    // Set {the text on the cell} with the {description of the item that is at the nth index of items},
+    //      where n = {row this cell will appear in on the tableview }
+    BNRItem *p = [[[BNRItemStore sharedStore] allItems]
+                  objectAtIndex:[indexPath row]];
+    
+    [[cell textLabel] setText:[p description]];
+    
+    return cell;    
+}
 @end
