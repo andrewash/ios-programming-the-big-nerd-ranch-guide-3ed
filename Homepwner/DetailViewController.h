a1a42ecb004370421e1d52d19e3ff9180d515c55
Andrew Ash <aash@zenmensch.com>
2013-04-14 14:25:55 -0400

    new Objective-C class DetailViewController, which inherits UIViewController   includes a new XIB file (these are the default files)

---
+++ Homepwner/DetailViewController.h
@@ -0,0 +1,9 @@
+//  DetailViewController.h
+//  Created by aash on 2013-04-14.
+
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ Homepwner/DetailViewController.m
@@ -0,0 +1,34 @@
+//  DetailViewController.m
+//  Created by aash on 2013-04-14.
+
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
