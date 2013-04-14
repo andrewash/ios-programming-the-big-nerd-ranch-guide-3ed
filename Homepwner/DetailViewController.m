115408996b09511a4edb01e4e4aa386159821da9
Andrew Ash <aash@zenmensch.com>
2013-04-14 14:27:51 -0400

    removed template code from .m file

--- Homepwner/DetailViewController.m
+++ Homepwner/DetailViewController.m
@@ -4,31 +4,7 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
-
-@end
-
 @implementation DetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
