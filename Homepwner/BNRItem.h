d610419943755c1045b8970654ec77c75573680e
Andrew Ash <enderash@gmail.com>
2013-04-15 19:48:24 -0400

    Allow controllers to modify the dateCreated field on this model

--- Homepwner/BNRItem.h
+++ Homepwner/BNRItem.h
@@ -24,6 +24,6 @@
 @property (nonatomic, copy) NSString *itemName;
 @property (nonatomic, copy) NSString *serialNumber;
 @property (nonatomic) int valueInDollars;
-@property (nonatomic, readonly, strong) NSDate *dateCreated;
+@property (nonatomic, strong) NSDate *dateCreated;
 
 @end
