a6683eb228dc655e7ab8d4fb5d91fb0e54b30410
Andrew Ash <enderash@gmail.com>
2013-04-22 19:55:33 -0400

    Nerdfeed app started, w/ empty application template (reading Ch. 25)

---
+++ ch25-26--Nerdfeed/Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by aash on 2013-04-22.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ch25-26--Nerdfeed/Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by aash on 2013-04-22.
+//  Copyright (c) 2013 Zen Mensch Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+}
