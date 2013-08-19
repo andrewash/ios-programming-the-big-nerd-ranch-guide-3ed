9937a91a21841a0e9885610bf75f59e4c4a30570
Andrew Ash <enderash@gmail.com>
2013-08-19 12:16:27 -0400

    WIP - CustomTableCell, etc. (Ch. 15??)

---
+++ Homepwner/Homepwner/HomepwnerItemCell.h
@@ -0,0 +1,9 @@
+//
+//  HomepwnerItemCell.h
+//  Created by Andrew Ash on 8/8/2013.
+
+#import <Foundation/Foundation.h>
+
+@interface HomepwnerItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/Homepwner/HomepwnerItemCell.m
@@ -0,0 +1,9 @@
+//
+//  HomepwnerItemCell.m
+//  Created by Andrew Ash on 8/8/2013.
+
+#import "HomepwnerItemCell.h"
+
+@implementation HomepwnerItemCell
+
+@end
